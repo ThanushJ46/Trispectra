481d15f3c0c973e6ef10d47bd670f1d7662ecb4b
Thanush J <mrthanush123@gmail.com>
2026-04-25 15:26:31 +0530

    Update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -36,8 +36,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
